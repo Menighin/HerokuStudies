b69b0723265252079559fd8989ebc019670059a0
Menighin <joao.menighin@gmail.com>
2021-03-28 02:04:38 -0300

    Now some changes

--- HerokuCICD/Controllers/WeatherForecastController.cs
+++ HerokuCICD/Controllers/WeatherForecastController.cs
@@ -31,7 +31,8 @@ namespace HerokuCICD.Controllers
             {
                 Date = DateTime.Now.AddDays(index),
                 TemperatureC = rng.Next(-20, 55),
-                Summary = Summaries[rng.Next(Summaries.Length)]
+                Summary = Summaries[rng.Next(Summaries.Length)],
+                TestingDeploy = "It worked!"
             })
             .ToArray();
         }
